03c89133134345dc02f1c4b110d2101d1ef04943
Gronify <thegronify@gmail.com>
2024-07-13 17:28:24 +0300

    ssr products cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.dummyjson.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'dummyjson.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+
+};
 
 export default nextConfig;
