c4155478f05ff5fa5da741d6f68c0345c4441d44
Gronify <thegronify@gmail.com>
2025-06-09 00:16:18 +0300

    fix:

--- src/app/ssg/recipes/[id]/page.tsx
+++ src/app/ssg/recipes/[id]/page.tsx
@@ -2,6 +2,7 @@ import type { Recipe } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
 import { fetchRecipe, fetchRecipes } from "../../api/api";
+import { notFound } from "next/navigation";
 
 interface RecipesPageProps {
   params: {
@@ -11,7 +12,9 @@ interface RecipesPageProps {
 
 const RecipePage = async ({ params }: RecipesPageProps) => {
   const recipe: Recipe = await fetchRecipe(params.id);
-
+  if (!recipe.ingredients) {
+    notFound();
+  }
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md">
--- src/app/ssr/recipes/[id]/page.tsx
+++ src/app/ssr/recipes/[id]/page.tsx
@@ -1,8 +1,8 @@
-import type { Recipe } from '@/types/types';
-import Image from 'next/image';
-import Link from 'next/link';
-import { fetchRecipe } from '../../api/api';
-
+import type { Recipe } from "@/types/types";
+import Image from "next/image";
+import Link from "next/link";
+import { fetchRecipe } from "../../api/api";
+import { notFound } from "next/navigation";
 
 interface RecipesPageProps {
   params: {
@@ -12,7 +12,9 @@ interface RecipesPageProps {
 
 const RecipePage = async ({ params }: RecipesPageProps) => {
   const recipe: Recipe = await fetchRecipe(params.id);
-
+  if (!recipe.ingredients) {
+    notFound();
+  }
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded-lg shadow-md">
@@ -20,16 +22,39 @@ const RecipePage = async ({ params }: RecipesPageProps) => {
           Back to Recipes
         </Link>
         <h1 className="text-2xl font-bold mb-4">{recipe.name}</h1>
-        <Image src={recipe.image} alt={recipe.name} width={360} height={360} className=" h-64 object-cover rounded-lg mb-4" />
+        <Image
+          src={recipe.image}
+          alt={recipe.name}
+          width={360}
+          height={360}
+          className=" h-64 object-cover rounded-lg mb-4"
+        />
         <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-          <p><strong>Prep Time:</strong> {recipe.prepTimeMinutes} minutes</p>
-          <p><strong>Cook Time:</strong> {recipe.cookTimeMinutes} minutes</p>
-          <p><strong>Servings:</strong> {recipe.servings}</p>
-          <p><strong>Difficulty:</strong> {recipe.difficulty}</p>
-          <p><strong>Cuisine:</strong> {recipe.cuisine}</p>
-          <p><strong>Calories per Serving:</strong> {recipe.caloriesPerServing}</p>
-          <p><strong>Rating:</strong> {recipe.rating} ({recipe.reviewCount} reviews)</p>
-          <p><strong>Meal Type:</strong> {recipe.mealType.join(', ')}</p>
+          <p>
+            <strong>Prep Time:</strong> {recipe.prepTimeMinutes} minutes
+          </p>
+          <p>
+            <strong>Cook Time:</strong> {recipe.cookTimeMinutes} minutes
+          </p>
+          <p>
+            <strong>Servings:</strong> {recipe.servings}
+          </p>
+          <p>
+            <strong>Difficulty:</strong> {recipe.difficulty}
+          </p>
+          <p>
+            <strong>Cuisine:</strong> {recipe.cuisine}
+          </p>
+          <p>
+            <strong>Calories per Serving:</strong> {recipe.caloriesPerServing}
+          </p>
+          <p>
+            <strong>Rating:</strong> {recipe.rating} ({recipe.reviewCount}{" "}
+            reviews)
+          </p>
+          <p>
+            <strong>Meal Type:</strong> {recipe.mealType.join(", ")}
+          </p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-4 mb-4">
           <h2 className="text-xl font-bold mb-2">Ingredients</h2>
@@ -48,8 +73,9 @@ const RecipePage = async ({ params }: RecipesPageProps) => {
           </ol>
         </div>
         <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-
-          <p><strong>Tags:</strong> {recipe.tags.join(', ')}</p>
+          <p>
+            <strong>Tags:</strong> {recipe.tags.join(", ")}
+          </p>
         </div>
       </div>
     </div>
